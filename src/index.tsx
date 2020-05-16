be35479f5bc2b64e71ded62d29b1e312c92e9fed
Peter Hayes <peter@remix.com>
2020-05-15 17:35:53 -0700

    initial commit

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,57 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+import image1 from './images/1.jpg';
+import image2 from './images/2.jpg';
+import image3 from './images/3.jpg';
+import image4 from './images/4.jpg';
+import image5 from './images/5.jpg';
+import image6 from './images/6.jpg';
+import image7 from './images/7.jpg';
+import image8 from './images/8.jpg';
+
+const IMAGES = [
+  image1,
+  image2,
+  image3,
+  image4,
+  image5,
+  image6,
+  image7,
+  image8,
+]
+
+function Container() {
+  return (
+    <div
+      style={{
+        position: 'fixed',
+        top: 0,
+        left: 0,
+        right: 0,
+        bottom: 0,
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}
+    >
+      <Image />
+    </div>
+  )
+}
+
+function Image() {
+  const index = Math.floor(Math.random() * IMAGES.length);
+  const image = IMAGES[index];
+
+  return (
+    <img src={image} style={{maxWidth: '80%', maxHeight: '80%'}} alt="nature!"/>
+  )
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Container />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
