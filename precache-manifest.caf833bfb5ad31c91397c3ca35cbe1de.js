ee0eaaac1533e23dc8900de575e2b85d4ef6a7de
Peter Hayes <peter@remix.com>
2020-05-15 17:36:23 -0700

    Updates

---
+++ precache-manifest.caf833bfb5ad31c91397c3ca35cbe1de.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95d49981f765991bfd965c63058b7e29",
+    "url": "/originoflife/index.html"
+  },
+  {
+    "revision": "ef78726fd079c623ad2c",
+    "url": "/originoflife/static/js/2.c4e92e98.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/originoflife/static/js/2.c4e92e98.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aad4ac4360e6ba32bae6",
+    "url": "/originoflife/static/js/main.11784366.chunk.js"
+  },
+  {
+    "revision": "3d77f230f03cbe3f3b2d",
+    "url": "/originoflife/static/js/runtime-main.724e3ae9.js"
+  },
+  {
+    "revision": "6227495ec12bfdddd8ccc4a8d028a39f",
+    "url": "/originoflife/static/media/1.6227495e.jpg"
+  },
+  {
+    "revision": "56cc4d684879bc5260ddfdad545d46d0",
+    "url": "/originoflife/static/media/2.56cc4d68.jpg"
+  },
+  {
+    "revision": "de17d57e0763d7bfdec6a0b75ce73169",
+    "url": "/originoflife/static/media/3.de17d57e.jpg"
+  },
+  {
+    "revision": "e4d9f08b75063435526ab2b065ef1371",
+    "url": "/originoflife/static/media/4.e4d9f08b.jpg"
+  },
+  {
+    "revision": "58f13f7738368e9c454b6c2e6ae79b9b",
+    "url": "/originoflife/static/media/5.58f13f77.jpg"
+  },
+  {
+    "revision": "1502c30b82d171585626751c4af1457b",
+    "url": "/originoflife/static/media/6.1502c30b.jpg"
+  },
+  {
+    "revision": "4e8adf0a7a58d7a50e7484fbf5099872",
+    "url": "/originoflife/static/media/7.4e8adf0a.jpg"
+  },
+  {
+    "revision": "03a7177bfaa77682d7c79edc3a20498a",
+    "url": "/originoflife/static/media/8.03a7177b.jpg"
+  }
+]);
